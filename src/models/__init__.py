fd4169912d2f14a552e06b750b22fe6a8f16d8e5
mr_eugene <roberttimethone@gmail.com>
2026-02-14 19:53:24 +0100

    created movie schemas

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
-from src.models.models import User, UserGroup, UserProfile, ActivationToken, PasswordResetToken, RefreshToken
+from src.models.user import User, UserGroup, UserProfile, ActivationToken, PasswordResetToken, RefreshToken
 from src.models.enums import UserGroupEnum, GenderEnum
-from src.models.movie_models import (
+from src.models.movie import (
     Genre, Star, Director, Certification, Movie,
     MovieLike, MovieComment, CommentLike, MovieFavorite, MovieRating,
     movie_genres, movie_directors, movie_stars
